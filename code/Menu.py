8f8cd0cf450ca8bd3a6bca6533d25ce28cb1c842
pombimsjb <itachisolid@gmail.com>
2024-04-02 21:52:33 -0300

    Fim da Aula Prática 02 -Text Menu

---
+++ code/Menu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH
+
+
+class Menu:
+    def __init__(self, window):
+        self.window: Surface = window
+        self.surf = pygame.image.load('./asset/img/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self):
+        pygame.mixer_music.load('./asset/sound/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Galatic", (255, 247, 252), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", (255, 247, 252), ((WIN_WIDTH / 2), 110))
+            pygame.display.flip()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color)
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
